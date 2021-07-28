5b0af773af224b6b1c57e8da574f1f05f221a938
Brandon Bates <b.bates@techsmith.com>
2021-07-27 23:33:03 -0400

    Parallelism, new output format, comment based help

--- audit_groups.ps1
+++ audit_groups.ps1
@@ -1,58 +1,109 @@
+<#
+.SYNOPSIS
+
+Gets all Azure Active Directory group memberships
+
+.DESCRIPTION
+
+The Get-AzureADReport.ps1 script exports all Azure Active Directory
+group memberships to a single CSV
+
+.PARAMETER ReportFilePath
+Specifies the name and path for the CSV-based output file. By default,
+Get-AzureADReport.ps1 generates a name from the date and time it runs, and
+saves the output in the users Desktop directory.
+
+.INPUTS
+
+None. You cannot pipe objects to Get-AzureADReport.ps1.
+
+.OUTPUTS
+
+None. Get-AzureADReport.ps1 does not generate any output.
+
+.EXAMPLE
+
+PS> .\Get-AzureADReport.ps1
+
+.EXAMPLE
+
+PS> .\Get-AzureADReport.ps1 -ReportFilePath C:\Data\January.csv
+
+#>
+
 param(
   [Parameter(Mandatory = $false)]
   [string]
-  $ReportFile = "c:\aipoint\report.csv"
+  $ReportFilePath
 )
 
-# This script creates a report based on members of all groups supplied by Azure...
-# This script assumes you have already used Connect-AzureAD...
-
 function Get-AzureADReport {
-  begin {
-    $groupTable = $null
-    $groupTable = @{}
+  [CmdletBinding()]
+  param(
+    [Parameter(Mandatory = $true)]
+    [string]
+    $ExportFilePath,
 
-    $groupTable2 = $null
-    $groupTable2 = @{}
+    [Parameter(Mandatory = $false, DontShow = $true)]
+    [Int32]
+    $DoP = 20 # Degree of Parallelism
+  )
+  try {
+    $aadGroups = Get-AzADGroup
+    $i = 0
+    $count = $aadGroups.Count
 
-    $infoGroup = Get-AzADGroup | Select-Object ObjectId, ObjectType, DisplayName
-
-    # Create the report file...
-    New-Item -Path $ReportFile -Force
-  }
-  process {
-    foreach ($groupId in $infoGroup) {
-      $groupTable.add(($groupId.ObjectId), ($groupId.Displayname))
-    }
-    foreach ($groupId in $infoGroup) {
-      $groupTable2.add(($groupId.ObjectId), ($groupId.ObjectType))
-    }
-  }
-  end {
-    foreach ($key in $groupTable.keys) {
-      try {
-        "" | Out-File -FilePath $ReportFile -Append
-        $newgroupline = '*-*-*-*-{0} ({1})-*-*-*-*' -f $groupTable[$key], $groupTable2[$key]
-        Out-File -FilePath $ReportFile -InputObject $newgroupline -Append
-        $memberships = Get-AzADGroupMember -ObjectId $key | Select-Object DisplayName, UserPrincipalName
-        foreach ($membership in $memberships) {
-          $newmembershipline = '{0},{1}' -f "$($membership.DisplayName)", "$($membership.UserPrincipalName)" 
-          Out-File -FilePath $ReportFile -InputObject $newmembershipline -Append
+    foreach ($group in $aadGroups) {
+      while ( (Get-Job | Where-Object -Property State -eq "Running").Count -eq $DoP ) { <# Wait for instance count to be less than DoP #> }
+      $completedJobs = Get-Job | Where-Object -Property State -eq "Completed"
+      if($completedJobs){
+        $completedJobs | ForEach-Object {
+          $results = $_ | Receive-Job
+          $results | Select-Object -Property GroupName, DisplayName, Id, Type, UserPrincipalName | Export-Csv -Path $ExportFilePath -Append -NoTypeInformation -Force
+          $_ | Remove-Job
         }
       }
-      catch {
-        Write-Host "[Whoops!] the script has ran into an error while generating the report! $_"
+
+      $i++
+      Write-Progress -Activity "($i/$count)" -Status "$($group.DisplayName)" -PercentComplete $(($i / $count) * 100)
+
+      Start-Job -Name $group.DisplayName -ScriptBlock {
+        $groupMembers = Get-AzADGroupMember -GroupObjectId $using:group.Id
+        foreach($member in $groupMembers){
+          $member | Add-Member -MemberType NoteProperty -Name GroupName -Value $using:group.DisplayName
+          $member | Select-Object -Property GroupName, DisplayName, Id, Type, UserPrincipalName
+        }
+      } | Out-Null
+    }
+  
+    While (Get-Job | Where-Object -Property State -eq "Running") { <# Wait For All Jobs To Complete #>> }
+    $completedJobs = Get-Job | Where-Object -Property State -eq "Completed"
+    if($completedJobs){
+      $completedJobs | ForEach-Object {
+        $results = $_ | Receive-Job
+        $results | Select-Object -Property GroupName, DisplayName, Id, Type, UserPrincipalName | Export-Csv -Path $ExportFilePath -Append -NoTypeInformation -Force
+        $_ | Remove-Job
       }
     }
+  }
+  catch {
+    Write-Error $Error[0].Exception
   }
 }
 
 function Main {
-  Write-Host "$(Get-Date -format 'u') [Begin] The report will be generated here: $ReportFile"
+  if($null -eq $ReportFilePath){
+    $date = (Get-Date -Format o -AsUTC) -replace (":",".")
+    $ReportFilePath = ("$home{0}Desktop{0}AzureADReport-$date.csv") -f [IO.Path]::DirectorySeparatorChar
+  }
 
-  Get-AzureADReport
+  if($null -eq (Get-AzContext)){
+    Connect-AzAccount
+  }
 
-  Write-Host "$(Get-Date -format 'u') [End] The report has been saved here: $ReportFile"
+  Write-Host "$(Get-Date -format 'u') [Begin] The report will be generated here: $ReportFilePath"
+  Get-AzureADReport -ExportFilePath $ReportFilePath
+  Write-Host "$(Get-Date -format 'u') [End] The report has been saved here: $ReportFilePath"
 }
 
 Main
